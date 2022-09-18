1e2a2dd219bca34ce8313a6de8e9cc3f0371ff32
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-09-18 08:50:08 -0300

    Ajustes

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import { Navbar } from "./Navbar";
 
 
 export const Layout = ({ children }) => {
 	return (
 		<>
+			<Navbar />
+
 			<div className="bg-gray-100  h-screen p-10">
 				<div className="container mx-auto w-3/5 h-1/2 my-10">{children}</div>
 			</div>
