4405a5da2107f109580d33f5a20d423d59d2a04b
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-09-20 04:51:18 -0300

    Ajustes

--- components/CategorySelect.jsx
+++ components/CategorySelect.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import { useContext } from 'react'
+import  CategoryContext  from './CategoryContext'
 
 
-export const CategorySelect = ({categories}) => {
+export const CategorySelect = () => {
+    const {categories, setCategories} = useContext(CategoryContext);
+
 	return (
+        <div className="flex flex-row">
         <select>{
             categories&&
             categories.map( (categoria) => <option value={categoria.id} key={categoria.id} >{categoria.nome}</option> )
         }
         </select>
+        <button>ADD</button>
+        </div>
     )
 }
