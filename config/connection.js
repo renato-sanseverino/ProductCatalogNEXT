f2633539dc5f1d3ba6a91bedcb27ab15d58c24ba
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-11-02 08:05:10 -0300

    Ajustes

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,17 @@
 import { PrismaClient } from '@prisma/client';
 
 
-// dados da conexão com BD
 const host     = 'localhost'
-const port     = 3306
-const username = 'root'
+const port     = 1433
+const username = 'sa'
 const password = 'p@ssw0rd'
 const database = 'commercedb'
 const ssl      = false
 const setSSL   = 'sslaccept=strict&sslmode=require'
 
 
-// DATABASE_URL="mysql://root:p@ssw0rd@localhost/commercedb"
-let url = `mysql://${username}:${password}@${host}/${database}`;
+// DATABASE_URL="sqlserver://localhost:1433;databaseName=commercedb;user=sa;password=p@ssw0rd;trustServerCertificate=true;"
+let url = `sqlserver://${host}:${port};databaseName=${database};user=${username};password=${password};trustServerCertificate=true;`;
 if (ssl) {
     url = url + `?${setSSL}`;
 }
